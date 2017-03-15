a4cf163a652680613ff3c81baeb92dbc2b995e29
aplested <a.plested@gmail.com>
2017-03-15 13:44:31 +0100

    defining custom cells for settings table view

---
+++ ISCAT/CustomSettingsCells.swift
@@ -0,0 +1,15 @@
+//
+//  CustomSettingsCells.swift
+//  ISCAT
+//
+//  Created by Andrew on 15/03/2017.
+//  Copyright © 2017 Andrew. All rights reserved.
+//
+
+import UIKit
+
+class CustomSettingCell: UITableViewCell {
+    @IBOutlet weak var myView: UIView!
+    @IBOutlet weak var SettingLabel: UILabel!
+    @IBOutlet weak var SettingValue: UILabel!
+}
